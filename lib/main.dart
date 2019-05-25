9368eac5381aee6068e9d888fb4e584010c2f93a
weversonluiz <weverson408@gmail.com>
2019-05-25 16:55:05 -0300

    configurando o path_provider, criando o metodo _getFile e salvar dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
@@ -12,6 +16,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+  //Salvar os dados
+  Future<File> _saveData() async {
+    String dados = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(dados);
+  }
+  //Ler o arquivo de dados
+  //Future evento que irá acontecer
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/dados_tarefa.json");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
